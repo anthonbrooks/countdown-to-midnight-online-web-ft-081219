2576c339611aa124fe32771627138d115c85b8e8
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-12 17:59:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,11 @@ def countdown(number)
     return "HAPPY NEW YEAR!"
 
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECONDS!"
+    sleep(1)
+    number -= 1
+  end
+end
