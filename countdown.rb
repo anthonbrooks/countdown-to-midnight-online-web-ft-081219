806c311a686a5660b210bf6592ac3e39c2ab0597
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-12 17:54:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,11 @@
 #write your code here
 
 def countdown(number)
-  while number > 0
+  while number >= 0
     puts "#{number} SECOND(S)!"
     number -= 1
+  end
+  if number == 0
+    puts "HAPPY NEW YEAR"
   end
 end
